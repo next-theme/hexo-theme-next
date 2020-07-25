f3a2a6f5e1e6579157b740e2015092a159927617
Mimi <1119186082@qq.com>
2020-07-25 19:13:28 +0800

    Update file name

--- test/tags/group-pictures.js
+++ test/tags/group-pictures.js
@@ -11,28 +11,28 @@ describe('group-pictures', () => {
 
   it('layout 3-1', () => {
     groupPicture(['3-1'], `
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div></div></div>');
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div></div></div>');
   });
 
   it('layout 5-2', () => {
     groupPicture(['5-2'], `
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 100%;"><img src="/images/docs/next.svg"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div></div></div>');
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 100%;"><img src="/images/sample.png"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div></div></div>');
   });
 
   it('no layout', () => {
     groupPicture(['NaN-NaN'], `
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)
-![](/images/docs/next.svg)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/docs/next.svg"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/docs/next.svg"></div></div></div>');
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)
+![](/images/sample.png)`).should.eql('<div class="group-picture"><div class="group-picture-row"><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 33.333333333333336%;"><img src="/images/sample.png"></div></div><div class="group-picture-row"><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div><div class="group-picture-column" style="width: 50%;"><img src="/images/sample.png"></div></div></div>');
   });
 });
--- test/tags/link-grid.js
+++ test/tags/link-grid.js
@@ -3,11 +3,11 @@
 require('chai').should();
 
 const result = `<div class="link-grid"><div class="link-grid-container">
-<div class="link-grid-image" style="background-image: url(/images/apple-touch-icon-next.png);"></div>
+<div class="link-grid-image" style="background-image: url(/images/sample.png);"></div>
 <p>Theme NexT</p><p>Stay Simple. Stay NexT.</p>
 <a href="https://theme-next.js.org/"></a>
 </div><div class="link-grid-container">
-<div class="link-grid-image" style="background-image: url(/images/apple-touch-icon-next.png);"></div>
+<div class="link-grid-image" style="background-image: url(/images/sample.png);"></div>
 <p>Theme NexT</p><p>Stay Simple. Stay NexT.</p>
 <a href="https://theme-next.js.org/"></a>
 </div></div>`;
@@ -17,26 +17,26 @@ describe('link-grid', () => {
 
   it('default', () => {
     linkGrid([], `
-Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/apple-touch-icon-next.png
-Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/apple-touch-icon-next.png`).should.eql(result);
+Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/sample.png
+Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/sample.png`).should.eql(result);
   });
 
   it('comment', () => {
     linkGrid([], `
-Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/apple-touch-icon-next.png
-Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/apple-touch-icon-next.png
-% Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/apple-touch-icon-next.png`).should.eql(result);
+Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/sample.png
+Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/sample.png
+% Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. | /images/sample.png`).should.eql(result);
   });
 
   it('default image', () => {
-    linkGrid(['/images/apple-touch-icon-next.png'], `
+    linkGrid(['/images/sample.png'], `
 Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. |
 Theme NexT | https://theme-next.js.org/ | Stay Simple. Stay NexT. |`).should.eql(result);
   });
 
   it('custom delimiter', () => {
-    linkGrid(['/images/apple-touch-icon-next.png', ','], `
-Theme NexT , https://theme-next.js.org/ , Stay Simple. Stay NexT. , /images/apple-touch-icon-next.png
-Theme NexT , https://theme-next.js.org/ , Stay Simple. Stay NexT. , /images/apple-touch-icon-next.png`).should.eql(result);
+    linkGrid(['/images/sample.png', ','], `
+Theme NexT , https://theme-next.js.org/ , Stay Simple. Stay NexT. , /images/sample.png
+Theme NexT , https://theme-next.js.org/ , Stay Simple. Stay NexT. , /images/sample.png`).should.eql(result);
   });
 });
