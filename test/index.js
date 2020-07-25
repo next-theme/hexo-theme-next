e72fdf4065a0b0c14f447f7fa0bb802fc9f94d62
Mimi <1119186082@qq.com>
2020-07-25 12:13:49 +0800

    Unit Testing using Mocha and Chai

---
+++ test/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('NexT', () => {
+  require('./validate');
+});
---
+++ test/validate/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const should = require('chai').should();
+
+describe('Validate', () => {
+  it('validate - config', () => {
+    const themeConfig = fs.readFileSync(path.join(__dirname, '../../_config.yml'));
+    should.not.throw(() => {
+      yaml.safeLoad(themeConfig);
+    });
+  });
+
+  it('validate - language', () => {
+    const languagesPath = path.join(__dirname, '../../languages');
+    should.not.throw(() => {
+      fs.readdirSync(languagesPath).forEach(lang => {
+        if (!lang.endsWith('.yml')) return;
+        const languagePath = path.join(languagesPath, lang);
+        yaml.safeLoad(fs.readFileSync(languagePath), {
+          filename: path.relative(__dirname, languagePath)
+        });
+      });
+    });
+  });
+});
