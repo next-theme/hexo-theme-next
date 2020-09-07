832824e5c126202cf7f9811b001f7f87f19307c3
Mimi <1119186082@qq.com>
2020-09-07 10:12:50 +0800

    Update

--- scripts/helpers/next-vendors.js
+++ scripts/helpers/next-vendors.js
@@ -26,8 +26,5 @@ hexo.extend.helper.register('js_vendors', function() {
   if (theme.pangu) {
     vendors.push('pangu');
   }
-  if (theme.nprogress && !theme.fancybox) {
-    vendors.push('jquery');
-  }
   return vendors;
 });
