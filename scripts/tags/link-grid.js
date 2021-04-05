662dd80fc56198deb6d1f4628539566de68af558
滑 <1395348685z@gmail.com>
2021-04-05 17:16:49 +0800

    Minor fixes on #226 (#240)
    * Fix head inject point
    
    * Switch `.link-grid-image` to `<img>`
    
    * Switch `.link-grid-image` back to `<object>`
    
    - Set `type` to `image/jpeg` for image without file extension

--- scripts/tags/link-grid.js
+++ scripts/tags/link-grid.js
@@ -8,8 +8,10 @@ module.exports = function([image = '/images/avatar.gif', delimiter = '|', commen
   const links = content.split('\n').filter(line => line.trim() !== '').map(line => {
     const item = line.split(delimiter).map(arg => arg.trim());
     if (item[0][0] === comment) return '';
+    const imageSource = item[3] || image;
+    const hasExtension = /\.[^/]+$/.test(imageSource);
     return `<div class="link-grid-container">
-<object class="link-grid-image" data="${item[3] || image}"></object>
+<object class="link-grid-image" ${hasExtension ? '' : 'type="image/jpeg" '}data="${imageSource}"></object>
 <p>${item[0]}</p><p>${item[2] || item[1]}</p>
 <a href="${item[1]}"></a>
 </div>`;
