67648eef823ea1f649bc2f4e0d5a3a832cd7f6a8
滑 <1395348685z@gmail.com>
2021-04-07 21:03:51 +0800

    Fix growingio
    - Not listen to Pjax related events since GrowingIO listens to URL change events on its own

--- source/js/third-party/analytics/growingio.js
+++ source/js/third-party/analytics/growingio.js
@@ -1,6 +1,10 @@
 /* global CONFIG, gio */
 
-document.addEventListener('page:loaded', () => {
-  gio('init', `${CONFIG.growingio_analytics}`, {});
-  gio('send');
-});
+if (!window.gio) {
+  window.gio = function() {
+    (window.gio.q = window.gio.q || []).push(arguments);
+  };
+}
+
+gio('init', `${CONFIG.growingio_analytics}`, {});
+gio('send');
