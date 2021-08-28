c377515fd7c5dfd82bec6663a37c0f564bbe323d
Mimi <1119186082@qq.com>
2021-08-28 15:49:05 +0800

    Fix image caption generated by pandoc with fancybox enabled

--- source/js/utils.js
+++ source/js/utils.js
@@ -49,7 +49,10 @@ NexT.utils = {
 
       const imageTitle = $image.attr('title') || $image.attr('alt');
       if (imageTitle) {
-        $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
+        // Do not append image-caption if pandoc has already created a figcaption
+        if (!$imageWrapLink.next('figcaption').length) {
+          $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
+        }
         // Make sure img title tag will show correctly in fancybox
         $imageWrapLink.attr('title', imageTitle).attr('data-caption', imageTitle);
       }
