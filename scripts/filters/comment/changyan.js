ec629a6b891d752e7d07fd83591c56a42fbb62f7
313720186@qq.com <313720186@qq.com>
2021-11-27 14:13:21 +0800

    Perf: use the existing gitalk_md5 function without loading any front-end md5 library.

--- scripts/filters/comment/changyan.js
+++ scripts/filters/comment/changyan.js
@@ -10,7 +10,7 @@ hexo.extend.filter.register('theme_inject', injects => {
   const config = hexo.theme.config.changyan;
   if (!config.enable || !config.appid || !config.appkey) return;
 
-  injects.comment.raw('changyan', '<div class="comments" id="SOHUCS"></div>', {}, { cache: true });
+  injects.comment.raw('changyan', '<div class="comments" id="SOHUCS" sid="{{gitalk_md5(page.title)}}" </div>', {}, {});
 
   injects.bodyEnd.file('changyan', path.join(hexo.theme_dir, 'layout/_third-party/comments/changyan.njk'));
 
@@ -24,12 +24,14 @@ hexo.extend.filter.register('theme_inject', injects => {
   injects.postMeta.raw('changyan', `
   {% if post.comments %}
   <span class="post-meta-item">
-    ${iconText('far fa-comment', 'changyan', '评论数')}
-     <a title="changyan" href="{{ url_for(post.path) }}#SOHUCS">
-       <span title="{{ post.title }}" class="cy_cmt_count"></span>
+    ${iconText('far fa-comment', 'changyan')}
+     <a title="{{ post.title }}" href="{{ url_for(post.path) }}#SOHUCS">
+       <span id="sourceId::{{gitalk_md5(post.title)}}" class="cy_cmt_count"></span>
      </a>
   </span>
   {% endif %}
-  `, {}, {});
-
+  `,
+  {},
+  {}
+  );
 });
--- source/js/third-party/comments/changyan.js
+++ source/js/third-party/comments/changyan.js
@@ -3,22 +3,7 @@
 document.addEventListener('page:loaded', () => {
   const { changyan } = CONFIG;
   const mainJs = 'https://cy-cdn.kuaizhan.com/upload/changyan.js';
-  const md5Js = 'https://cdn.jsdelivr.net/npm/md5js@1.0.7/dist/md5.min.js';
   const countJs = `https://cy-cdn.kuaizhan.com/upload/plugins/plugins.list.count.js?clientId=${changyan.appid}`;
-
-  /**
-   *  Calculate comment MD5 by title
-   **/
-  const titleToMd5 = className => {
-    const { md5 : _md5 } = window.md5;
-    if (typeof _md5 === 'function') {
-      const posts = document.getElementsByClassName(className);
-      Array.from(posts).forEach(post => {
-        const dataMd5 = _md5(post.getAttribute('title'));
-        post.setAttribute('id', `sourceId::${dataMd5}`);
-      });
-    }
-  };
 
   /**
    *  Get number of comments
@@ -35,15 +20,9 @@ document.addEventListener('page:loaded', () => {
     );
   };
 
-  const addMd5ForChangYan = () => {
-    const { md5 : _md5 } = window.md5;
-    const wrap = document.getElementById('SOHUCS');
-    if (wrap && typeof _md5 === 'function') {
-      wrap.setAttribute('sid', _md5(CONFIG.page.title));
-    } else {
-      throw new Error('failed to  calculate title md5');
-    }
-  };
+  /**
+   * load changyan js
+   */
   const loadChangYan = () => {
     return NexT.utils.getScript(
       mainJs,
@@ -55,6 +34,9 @@ document.addEventListener('page:loaded', () => {
     );
   };
 
+  /**
+   *  fetch comments of post
+   */
   const loadComments = () => {
     const initComment = window.changyan?.api?.config;
     if (typeof initComment === 'function') {
@@ -67,25 +49,13 @@ document.addEventListener('page:loaded', () => {
     }
   };
 
-  const handleComments = () => {
-    titleToMd5('cy_cmt_count');
+  // get number of comments of post
+  setTimeout(getCommentsNum, 0);
 
-    // Join the task queue because chrome may be asynchronous to DOM operations
-    setTimeout(getCommentsNum, 0);
-    if (CONFIG.page.comments && !CONFIG.page.isHome) {
-      NexT.utils.loadComments('#SOHUCS')
-        .then(addMd5ForChangYan)
-        .then(loadChangYan)
-        .then(loadComments);
-    }
-  };
-
-  NexT.utils.getScript(
-    md5Js,
-    {
-      attributes: {
-        async: true
-      }
-    }
-  ).then(handleComments);
+  // when scroll to comment section
+  if (CONFIG.page.comments && !CONFIG.page.isHome) {
+    NexT.utils.loadComments('#SOHUCS')
+      .then(loadChangYan)
+      .then(loadComments);
+  }
 });
