704a5d80ffcde80709a76d6b57dc97d2233d9824
Mimi <1119186082@qq.com>
2022-01-01 03:21:15 +0800

    Code style update

--- scripts/filters/comment/changyan.js
+++ scripts/filters/comment/changyan.js
@@ -10,7 +10,7 @@ hexo.extend.filter.register('theme_inject', injects => {
   const config = hexo.theme.config.changyan;
   if (!config.enable || !config.appid || !config.appkey) return;
 
-  injects.comment.raw('changyan', '<div class="comments" id="SOHUCS" sid="{{gitalk_md5(page.title)}}"> </div>', {}, {});
+  injects.comment.raw('changyan', '<div class="comments" id="SOHUCS" sid="{{ gitalk_md5(page.title) }}"></div>', {}, {});
 
   injects.bodyEnd.file('changyan', path.join(hexo.theme_dir, 'layout/_third-party/comments/changyan.njk'));
 
@@ -26,12 +26,9 @@ hexo.extend.filter.register('theme_inject', injects => {
   <span class="post-meta-item">
     ${iconText('far fa-comment', 'changyan')}
      <a title="{{ post.title }}" href="{{ url_for(post.path) }}#SOHUCS">
-       <span id="sourceId::{{gitalk_md5(post.title)}}" class="cy_cmt_count"></span>
+       <span id="sourceId::{{ gitalk_md5(post.title) }}" class="cy_cmt_count"></span>
      </a>
   </span>
   {% endif %}
-  `,
-  {},
-  {}
-  );
+  `, {}, {});
 });
--- source/js/third-party/comments/changyan.js
+++ source/js/third-party/comments/changyan.js
@@ -6,56 +6,50 @@ document.addEventListener('page:loaded', () => {
   const countJs = `https://cy-cdn.kuaizhan.com/upload/plugins/plugins.list.count.js?clientId=${changyan.appid}`;
 
   /**
-   *  Get number of comments
+   * Get number of comments
    */
   const getCommentsNum = () => {
-    return NexT.utils.getScript(
-      countJs,
-      {
-        attributes: {
-          async: true,
-          id   : 'cy_cmt_num'
-        }
+    return NexT.utils.getScript(countJs, {
+      attributes: {
+        async: true,
+        id   : 'cy_cmt_num'
       }
-    );
+    });
   };
 
   /**
-   * load changyan js
+   * Load changyan js
    */
   const loadChangYan = () => {
-    return NexT.utils.getScript(
-      mainJs,
-      {
-        attributes: {
-          async: true
-        }
+    return NexT.utils.getScript(mainJs, {
+      attributes: {
+        async: true
       }
-    );
+    });
   };
 
   /**
-   *  fetch comments of post
+   * Fetch comments of post
    */
   const loadComments = () => {
-    const initComment = window.changyan?.api?.config;
-    if (typeof initComment === 'function') {
-      initComment({
-        appid: changyan.appid,
-        conf : changyan.appkey
-      });
-    } else {
-      throw new Error('changyan.js don\'t be loaded, please to https://github.com/next-theme/hexo-theme-next/issues to report a bug');
-    }
+    const initComment = window.changyan.api.config;
+    initComment({
+      appid: changyan.appid,
+      conf : changyan.appkey
+    });
   };
 
-  // get number of comments of post
+  // Get number of comments of post
   setTimeout(getCommentsNum, 0);
 
-  // when scroll to comment section
+  // When scroll to comment section
   if (CONFIG.page.comments && !CONFIG.page.isHome) {
     NexT.utils.loadComments('#SOHUCS')
       .then(loadChangYan)
-      .then(loadComments);
+      .then(loadComments)
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load Changyan', error);
+      });
   }
 });
