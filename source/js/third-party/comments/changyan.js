3f613d8eb37ec11f6a9d80625d93296e7e151c48
Mimi <1119186082@qq.com>
2022-01-01 03:42:26 +0800

    Code style update

--- source/js/third-party/comments/changyan.js
+++ source/js/third-party/comments/changyan.js
@@ -5,47 +5,32 @@ document.addEventListener('page:loaded', () => {
   const mainJs = 'https://cy-cdn.kuaizhan.com/upload/changyan.js';
   const countJs = `https://cy-cdn.kuaizhan.com/upload/plugins/plugins.list.count.js?clientId=${appid}`;
 
-  /**
-   * Get number of comments
-   */
-  const getCommentsNum = () => {
+  // Get the number of comments
+  setTimeout(() => {
     return NexT.utils.getScript(countJs, {
       attributes: {
         async: true,
         id   : 'cy_cmt_num'
       }
     });
-  };
-
-  /**
-   * Load changyan js
-   */
-  const loadChangYan = () => {
-    return NexT.utils.getScript(mainJs, {
-      attributes: {
-        async: true
-      }
-    });
-  };
-
-  /**
-   * Fetch comments of post
-   */
-  const loadComments = () => {
-    window.changyan.api.config({
-      appid: appid,
-      conf : appkey
-    });
-  };
-
-  // Get number of comments of post
-  setTimeout(getCommentsNum, 0);
+  }, 0);
 
   // When scroll to comment section
   if (CONFIG.page.comments && !CONFIG.page.isHome) {
     NexT.utils.loadComments('#SOHUCS')
-      .then(loadChangYan)
-      .then(loadComments)
+      .then(() => {
+        return NexT.utils.getScript(mainJs, {
+          attributes: {
+            async: true
+          }
+        });
+      })
+      .then(() => {
+        window.changyan.api.config({
+          appid,
+          conf: appkey
+        });
+      })
       .catch(error => {
         // eslint-disable-next-line no-console
         console.error('Failed to load Changyan', error);
