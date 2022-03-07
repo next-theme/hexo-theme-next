2ca7577f0a50bbb565ae9d8adb9744e130374c51
Mimi <1119186082@qq.com>
2022-03-07 14:37:53 +0800

    Use insertAdjacentHTML instead of innerHTML

--- source/js/schedule.js
+++ source/js/schedule.js
@@ -121,10 +121,10 @@
         }
 
         if (tense === 'future' && prevEnd < now) {
-          eventList.innerHTML += '<hr>';
+          eventList.insertAdjacentHTML('beforeend', '<hr>');
         }
 
-        eventList.innerHTML += buildEventDOM(tense, event, start, end);
+        eventList.insertAdjacentHTML('beforeend', buildEventDOM(tense, event, start, end));
         prevEnd = end;
       });
     });
