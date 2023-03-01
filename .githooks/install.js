55a0ede58755cae4b9ef223d4c5b1f010298d734
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-03-01 13:13:33 +0700

    Using absolute path githooks (#616)

--- .githooks/install.js
+++ .githooks/install.js
@@ -1,6 +1,7 @@
 const { spawn } = require('child_process');
+const path = require('path');
 
-const subprocess = spawn('git', ['config', '--local', 'core.hooksPath', '.githooks/']);
+const subprocess = spawn('git', ['config', '--local', 'core.hooksPath', path.join(__dirname, '.githooks/')]);
 
 subprocess.on('error', () => {
   console.error('Failed to install git hook.');
