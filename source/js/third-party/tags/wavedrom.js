f5a3130b51bdf54ecca2e8c237e6a7a5c250018b
r12f <r12f.code@gmail.com>
2023-05-21 09:30:22 -0700

    Fix lint.

--- source/js/third-party/tags/wavedrom.js
+++ source/js/third-party/tags/wavedrom.js
@@ -1,11 +1,11 @@
-/* global NexT, CONFIG, wavedrom */
+/* global NexT, CONFIG, WaveDrom */
 
 document.addEventListener('page:loaded', () => {
-    NexT.utils.getScript(CONFIG.wavedrom.js, {
-      condition: window.wavedrom
-    }).then(() => {
-        NexT.utils.getScript(CONFIG.wavedrom_skin.js).then(() => {
-            WaveDrom.ProcessAll()
-        });
+  NexT.utils.getScript(CONFIG.wavedrom.js, {
+    condition: window.WaveDrom
+  }).then(() => {
+    NexT.utils.getScript(CONFIG.wavedrom_skin.js).then(() => {
+      WaveDrom.ProcessAll();
     });
+  });
 });
