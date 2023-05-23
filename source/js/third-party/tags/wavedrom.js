be685394cd89d8ef0a802261c36e4020656d21c4
r12f <r12f.code@gmail.com>
2023-05-23 00:32:29 -0700

    Fix wavedrom skin load condition.

--- source/js/third-party/tags/wavedrom.js
+++ source/js/third-party/tags/wavedrom.js
@@ -4,7 +4,9 @@ document.addEventListener('page:loaded', () => {
   NexT.utils.getScript(CONFIG.wavedrom.js, {
     condition: window.WaveDrom
   }).then(() => {
-    NexT.utils.getScript(CONFIG.wavedrom_skin.js).then(() => {
+    NexT.utils.getScript(CONFIG.wavedrom_skin.js, {
+      condition: window.WaveSkin
+    }).then(() => {
       WaveDrom.ProcessAll();
     });
   });
