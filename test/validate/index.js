40d64a465ff5c911f74d8bc77e77147cb3337ec9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-12-03 06:42:47 -0500

    fix lint
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- test/validate/index.js
+++ test/validate/index.js
@@ -13,19 +13,19 @@ describe('Validate', () => {
     });
   });
 
-  it('vendors', async () => {
+  it('vendors', async() => {
     const { createHash } = require('crypto');
     const { getVendors } = require('../../scripts/events/lib/utils');
     const vendorsFile = fs.readFileSync(path.join(__dirname, '../../_vendors.yml'));
-    (await Promise.all(Object.entries(yaml.load(vendorsFile)).map(async ([key, vendor]) => {
+    (await Promise.all(Object.entries(yaml.load(vendorsFile)).map(async([key, vendor]) => {
       vendor.minified = vendor.file || '';
       const { cdnjs } = getVendors(vendor);
       // fetch content and check sha256
       return await fetch(cdnjs)
-        .then((response) => response.text())
-        .then((body) => {
+        .then(response => response.text())
+        .then(body => {
           const integrity = Buffer.from(createHash('sha256').update(body, 'utf8').digest()).toString('base64');
-          return !vendor.integrity || "sha256-" + integrity == vendor.integrity;
+          return !vendor.integrity || 'sha256-' + integrity === vendor.integrity;
         });
     }))).should.all.equal(true);
   });
