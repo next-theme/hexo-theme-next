1ca10776ff8fc84c240e8ce74a464eee98d40f58
Mimi <1119186082@qq.com>
2024-05-07 16:43:11 +0800

    Refactor Algolia search: drop instantsearch.js (#795)

--- source/js/third-party/search/algolia-search.js
+++ source/js/third-party/search/algolia-search.js
@@ -1,112 +1,104 @@
-/* global instantsearch, algoliasearch, CONFIG, pjax */
+/* global CONFIG, NexT, pjax, algoliasearch */
 
 document.addEventListener('DOMContentLoaded', () => {
   const { indexName, appID, apiKey, hits } = CONFIG.algolia;
+  const client = algoliasearch(appID, apiKey);
+  const index = client.initIndex(indexName);
 
-  const search = instantsearch({
-    indexName,
-    searchClient  : algoliasearch(appID, apiKey),
-    searchFunction: helper => {
-      if (document.querySelector('.search-input').value) {
-        helper.search();
-      }
+  const input = document.querySelector('.search-input');
+  const container = document.querySelector('.search-result-container');
+
+  const formatHits = data => {
+    const { title, excerpt, excerptStrip, contentStripTruncate } = data._highlightResult;
+    let result = `<li><a href="${data.permalink}" class="search-result-title">${title.value}</a>`;
+    const content = excerpt || excerptStrip || contentStripTruncate;
+    if (content && content.value) {
+      const div = document.createElement('div');
+      div.innerHTML = content.value;
+      result += `<a href="${data.permalink}"><p class="search-result">${div.textContent.substring(0, 100)}...</p></a></li>`;
     }
-  });
+    return result;
+  };
 
-  if (typeof pjax === 'object') {
-    search.on('render', () => {
-      pjax.refresh(document.querySelector('.algolia-hits'));
+  let isSearching = false;
+  let pendingQuery = null;
+
+  const searchAlgolia = async(searchText, page = 0) => {
+    if (isSearching) {
+      pendingQuery = { searchText, page };
+      return;
+    }
+    isSearching = true;
+    const startTime = Date.now();
+    const data = await index.search(searchText, {
+      page,
+      attributesToRetrieve : ['permalink'],
+      attributesToHighlight: ['title', 'excerpt', 'excerptStrip', 'contentStripTruncate'],
+      hitsPerPage          : hits.per_page || 10,
+      highlightPreTag      : '<mark class="search-keyword">',
+      highlightPostTag     : '</mark>'
     });
-  }
-
-  // Registering Widgets
-  search.addWidgets([
-    instantsearch.widgets.configure({
-      hitsPerPage: hits.per_page || 10
-    }),
-
-    instantsearch.widgets.searchBox({
-      container           : '.search-input-container',
-      placeholder         : CONFIG.i18n.placeholder,
-      // Hide default icons of algolia search
-      showReset           : false,
-      showSubmit          : false,
-      showLoadingIndicator: false,
-      cssClasses          : {
-        input: 'search-input'
-      }
-    }),
-
-    instantsearch.widgets.stats({
-      container: '.algolia-stats',
-      templates: {
-        text: data => {
-          const stats = CONFIG.i18n.hits_time
-            .replace('${hits}', data.nbHits)
-            .replace('${time}', data.processingTimeMS);
-          return `<span>${stats}</span>
-            <img src="${CONFIG.images}/logo-algolia-nebula-blue-full.svg" alt="Algolia">`;
-        }
-      },
-      cssClasses: {
-        text: 'search-stats'
-      }
-    }),
-
-    instantsearch.widgets.hits({
-      container : '.algolia-hits',
-      escapeHTML: false,
-      templates : {
-        item: data => {
-          const { title, excerpt, excerptStrip, contentStripTruncate } = data._highlightResult;
-          let result = `<a href="${data.permalink}" class="search-result-title">${title.value}</a>`;
-          const content = excerpt || excerptStrip || contentStripTruncate;
-          if (content && content.value) {
-            const div = document.createElement('div');
-            div.innerHTML = content.value;
-            result += `<a href="${data.permalink}"><p class="search-result">${div.textContent.substring(0, 100)}...</p></a>`;
+    if (data.nbHits === 0) {
+      container.innerHTML = '<div class="search-result-icon"><i class="far fa-frown fa-5x"></i></div>';
+    } else {
+      const stats = CONFIG.i18n.hits_time
+        .replace('${hits}', data.nbHits)
+        .replace('${time}', Date.now() - startTime);
+      let pagination = '';
+      if (data.nbPages > 1) {
+        pagination += '<nav class="pagination algolia-pagination">';
+        for (let i = 0; i < data.nbPages; i++) {
+          if (i === page) {
+            pagination += `<span class="page-number current">${i + 1}</span>`;
+          } else {
+            pagination += `<a class="page-number" href="#" data-index=${i}>${i + 1}</a>`;
           }
-          return result;
-        },
-        empty: data => {
-          return `<div class="algolia-hits-empty">
-              ${CONFIG.i18n.empty.replace('${query}', data.query)}
-            </div>`;
         }
-      },
-      cssClasses: {
-        list: 'search-result-list'
+        pagination += '</nav>';
       }
-    }),
 
-    instantsearch.widgets.pagination({
-      container: '.algolia-pagination',
-      scrollTo : false,
-      showFirst: false,
-      showLast : false,
-      templates: {
-        first   : '<i class="fa fa-angle-double-left"></i>',
-        last    : '<i class="fa fa-angle-double-right"></i>',
-        previous: '<i class="fa fa-angle-left"></i>',
-        next    : '<i class="fa fa-angle-right"></i>'
-      },
-      cssClasses: {
-        list        : ['pagination', 'algolia-pagination'],
-        item        : 'pagination-item',
-        link        : 'page-number',
-        selectedItem: 'current',
-        disabledItem: 'disabled-item'
-      }
-    })
-  ]);
+      container.innerHTML = `<div class="search-stats">
+          <span>${stats}</span>
+          <img src="${CONFIG.images}/logo-algolia-nebula-blue-full.svg" alt="Algolia">
+        </div>
+        <hr>
+        <ul class="search-result-list">${data.hits.map(formatHits).join('')}</ul>
+        ${pagination}`;
+      if (typeof pjax === 'object') pjax.refresh(container);
+      container.querySelectorAll('.page-number').forEach(element => {
+        element.addEventListener('click', async event => {
+          event.preventDefault();
+          await searchAlgolia(searchText, Number(element.dataset.index));
+        });
+      });
+    }
+    isSearching = false;
+    if (pendingQuery !== null && (pendingQuery.searchText !== searchText || pendingQuery.page !== page)) {
+      const { searchText, page } = pendingQuery;
+      pendingQuery = null;
+      searchAlgolia(searchText, page);
+    }
+  };
 
-  search.start();
+  const inputEventFunction = async() => {
+    const searchText = input.value.trim();
+    if (searchText === '') {
+      container.innerHTML = '<div class="search-result-icon"><i class="fab fa-algolia fa-5x"></i></div>';
+      return;
+    }
+    // Algolia client will automatically cache the data for same queries
+    await searchAlgolia(searchText, 0);
+  };
+
+  const debouncedSearch = NexT.utils.debounce(inputEventFunction, 500);
+  input.addEventListener('input', debouncedSearch);
 
   // Handle and trigger popup window
   document.querySelectorAll('.popup-trigger').forEach(element => {
     element.addEventListener('click', () => {
       document.body.classList.add('search-active');
-      setTimeout(() => document.querySelector('.search-input').focus(), 500);
+      // Wait for search-popup animation to complete
+      setTimeout(() => input.focus(), 500);
     });
   });
 
--- source/js/third-party/search/local-search.js
+++ source/js/third-party/search/local-search.js
@@ -13,22 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   const input = document.querySelector('.search-input');
+  const container = document.querySelector('.search-result-container');
 
   const inputEventFunction = () => {
     if (!localSearch.isfetched) return;
     const searchText = input.value.trim().toLowerCase();
     const keywords = searchText.split(/[-\s]+/);
-    const container = document.querySelector('.search-result-container');
     let resultItems = [];
     if (searchText.length > 0) {
       // Perform local searching
       resultItems = localSearch.getResultItems(keywords);
     }
     if (keywords.length === 1 && keywords[0] === '') {
-      container.classList.add('no-result');
       container.innerHTML = '<div class="search-result-icon"><i class="fa fa-search fa-5x"></i></div>';
     } else if (resultItems.length === 0) {
-      container.classList.add('no-result');
       container.innerHTML = '<div class="search-result-icon"><i class="far fa-frown fa-5x"></i></div>';
     } else {
       resultItems.sort((left, right) => {
@@ -41,7 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
       });
       const stats = CONFIG.i18n.hits.replace('${hits}', resultItems.length);
 
-      container.classList.remove('no-result');
       container.innerHTML = `<div class="search-stats">${stats}</div>
         <hr>
         <ul class="search-result-list">${resultItems.map(result => result.item).join('')}</ul>`;
