213e619fb4419c090079d45d024734c8611cf307
Lucien <1358046425@qq.com>
2025-09-05 01:33:46 +0800

    remove:删除无用文件

--- .githooks/install.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-const path = require('path');
-
-const subprocess = spawn('git', ['config', '--local', 'core.hooksPath', path.join(__dirname, '.githooks/')]);
-
-subprocess.on('error', () => {
-  console.error('Failed to install git hook.');
-});
