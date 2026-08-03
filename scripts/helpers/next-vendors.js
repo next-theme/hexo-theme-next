850c2d8c8f94bd827bd75dcad5955adb5c05be67
Mimi <1119186082@qq.com>
2026-08-03 17:58:36 +0800

    Replace Anime.js with native animations

--- scripts/helpers/next-vendors.js
+++ scripts/helpers/next-vendors.js
@@ -4,7 +4,7 @@
 
 hexo.extend.helper.register('js_vendors', function() {
   const { config, theme } = this;
-  const vendors = ['anime'];
+  const vendors = [];
   if (theme.prism.enable && !config.prismjs.preprocess) {
     vendors.push('prism', 'prism_autoloader');
     if (config.prismjs.line_number) {
