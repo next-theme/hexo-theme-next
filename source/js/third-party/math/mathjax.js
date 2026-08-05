c4209ab9c4cbff32393cd65ea6134cfabc74307d
Mimi <1119186082@qq.com>
2026-08-05 10:53:53 +0800

    Configure MathJax overflow handling

--- source/js/third-party/math/mathjax.js
+++ source/js/third-party/math/mathjax.js
@@ -4,6 +4,12 @@ document.addEventListener('page:loaded', () => {
   if (!CONFIG.enableMath) return;
 
   if (typeof MathJax === 'undefined') {
+    const output = {
+      displayOverflow: CONFIG.mathjax.display_overflow
+    };
+    if (CONFIG.mathjax.font_path) {
+      output.fontPath = CONFIG.mathjax.font_path;
+    }
     window.MathJax = {
       tex: {
         inlineMath: { '[+]': [['$', '$']] },
@@ -12,7 +18,7 @@ document.addEventListener('page:loaded', () => {
       options: {
         renderActions: {
           insertedScript: [200, () => {
-            document.querySelectorAll('mjx-container').forEach(node => {
+            document.querySelectorAll('mjx-container:not([display])').forEach(node => {
               const target = node.parentNode;
               if (target.nodeName.toLowerCase() === 'li') {
                 target.parentNode.classList.add('has-jax');
@@ -21,9 +27,7 @@ document.addEventListener('page:loaded', () => {
           }, '', false]
         }
       },
-      output: CONFIG.mathjax.font_path ? {
-        fontPath: CONFIG.mathjax.font_path
-      } : {}
+      output
     };
     NexT.utils.getScript(CONFIG.mathjax.js, {
       attributes: {
